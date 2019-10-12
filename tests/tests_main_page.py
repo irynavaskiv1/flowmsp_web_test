e2686713723025755072577bf60f451887e4178e
Iryna Vaskiv <>
2019-10-12 12:54:13 +0300

    finished burger menu test cases

--- tests/tests_main_page.py
+++ tests/tests_main_page.py
@@ -5,7 +5,7 @@ from tests.test_base import BaseSelenium
 class BurgerMenuTestCase(BaseSelenium):
 
     def testing_if_exist_burger_menu(self):
-        """тестуємо чи є бургер меню кнопка"""
+        """testing if exist burger button"""
         self.login()
         time.sleep(2)
         burger_button = self.selenium.find_element_by_xpath(
@@ -22,19 +22,23 @@ class BurgerMenuTestCase(BaseSelenium):
         self.assertEquals(set_en, li_all_words)
 
     def testing_flow_button(self):
-        """тестуємо flow кнопку"""
+        """testing flow button"""
         self.login()
         flow_button = self.selenium.find_element_by_id('info-tabs-tab-1')
         flow_button.click()
         time.sleep(2)
-        info_flow_panel = self.selenium.find_element_by_id('info-pane-1')
+        info_flow_panel = self.selenium.find_element_by_class_name('tab-content')
         set_text = set(info_flow_panel.text.split('\n'))
-        set_words = {'Icon Legend', 'Flow Data', 'Hydrant Information'}
+        set_words = {'Icon Legend', 'Flow Data', 'Hydrant Information',
+                     'Dry Hydrant', '1500+ GPM', '1000 to less than 1500 GPM',
+                     'Unknown', 'Selected Hydrants: 0',
+                     'Total Flow Rate: 0 gal/min', 'No hydrant selected.',
+                     '500 to less than 1000 GPM', '0 to less than 500 GPM'}
         time.sleep(3)
         self.assertEquals(set_words, set_text)
 
-    def testing_location_menu(self):
-        """тестуємо location кнопку"""
+    def testing_location_button(self):
+        """testing location button"""
         self.login()
         location_button = self.selenium.find_element_by_id('info-tabs-tab-2')
         location_button.click()
@@ -42,12 +46,14 @@ class BurgerMenuTestCase(BaseSelenium):
         info_location_panel = self.selenium.find_element_by_id(
             'info-tabs-pane-2')
         set_text = set(info_location_panel.text.split('\n'))
-        set_words = {'Location Data', 'Flow Data', 'Hydrant Information'}
+        set_words = {'Location Data', 'Flow Data', 'Hydrant Information',
+                     'Selected Hydrants: 0', 'No location selected.',
+                     'Total Flow Rate: 0 gal/min', 'No hydrant selected.'}
         time.sleep(3)
         self.assertEquals(set_words, set_text)
 
-    def testing_building_data(self):
-        """тестуємо building кнопку"""
+    def testing_building_data_button(self):
+        """testing building button"""
         self.login()
         building_data_button = self.selenium.find_element_by_id(
             'info-tabs-tab-3')
@@ -56,7 +62,20 @@ class BurgerMenuTestCase(BaseSelenium):
         info_building_panel = self.selenium.find_element_by_id(
             'info-tabs-pane-3')
         set_text = set(info_building_panel.text.split('\n'))
-        set_words = {'Building Data'}
+        set_words = {'Building Data', 'No building info.'}
+        time.sleep(3)
+        self.assertEquals(set_words, set_text)
+
+    def testing_dispatch_button(self):
+        self.login()
+        dispatch_button = self.selenium.find_element_by_id(
+            'info-tabs-tab-4')
+        dispatch_button.click()
+        time.sleep(2)
+        info_dispatch_panel = self.selenium.find_element_by_id(
+            'info-tabs-pane-4')
+        set_text = set(info_dispatch_panel.text.split('\n'))
+        set_words = {'No Messages.'}
         time.sleep(3)
         self.assertEquals(set_words, set_text)
 
