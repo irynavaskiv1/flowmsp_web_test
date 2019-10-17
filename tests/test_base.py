c2110cd17aad903ae87bbfb6e0b083c0a9529b25
Iryna Vaskiv <>
2019-10-17 22:54:51 +0300

    last update base module

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,34 +1,35 @@
 import unittest
-import time
+from time import sleep
 from selenium import webdriver
-from constants import password, login
+from constants import LOGIN, PASSWORD
 
 
 class BaseSelenium(unittest.TestCase):
 
     def setUp(self):
         self.selenium = webdriver.Firefox()
-        self.selenium.get("https://app.flowmsp.com/login")
-        time.sleep(2)
+        self.selenium.get(
+            '{}{}'.format('https://app.flowmsp.com', '/login'))
+        sleep(2)
 
     def login(self):
         username_field = self.selenium.find_element_by_xpath(
             '/html/body/div/div/div/div[1]/div[1]/div/div/div/div[3]/div/div/'
             'form/div[1]/input')
-        username_field.send_keys(login)
-        time.sleep(3)
+        username_field.send_keys(LOGIN)
+        sleep(3)
 
         password_field = self.selenium.find_element_by_xpath(
             '/html/body/div/div/div/div[1]/div[1]/div/div/div/div[3]/div/div/'
             'form/div[2]/input')
-        password_field.send_keys(password)
-        time.sleep(3)
+        password_field.send_keys(PASSWORD)
+        sleep(3)
 
         login_button = self.selenium.find_element_by_xpath(
             '/html/body/div/div/div/div[1]/div[1]/div/div/div/div[3]/div/div/'
             'form/div[3]/input')
         login_button.click()
-        time.sleep(3)
+        sleep(3)
 
     def get_burger_button(self):
         return self.selenium.find_element_by_xpath(
@@ -52,18 +53,21 @@ class MyProfileBaseTestCase(BaseSelenium):
         self.selenium = webdriver.Firefox()
         self.selenium.get(
             '{}{}'.format('https://app.flowmsp.com', '/login'))
-        time.sleep(2)
+        sleep(2)
 
     def login_to_profile(self):
         self.login()
+        sleep(2)
         self.get_burger_button().click()
+        sleep(2)
         self.get_my_profile_button().click()
+        sleep(2)
 
     def get_edit_profile_button(self):
         return self.selenium.find_element_by_xpath(
             '/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/button[1]')
 
-    def get_change_password_buttton(self):
+    def get_change_password_button(self):
         return self.selenium.find_element_by_xpath(
             '/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/button[2]')
 
