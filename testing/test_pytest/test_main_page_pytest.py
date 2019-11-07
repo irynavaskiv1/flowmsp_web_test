b7a40e4a6a4c58252493385c8cc11b10aaa60d74
Iryna <irynavaskiv875@gmail.com>
2019-11-07 14:24:17 +0200

    update for correctly pytest

--- testing/test_pytest/test_main_page_pytest.py
+++ testing/test_pytest/test_main_page_pytest.py
@@ -1,15 +1,15 @@
-# from testing.test_pytest.test_base_pytest import BaseSelenium
-#
-#
-# class MainPageTestCase(BaseSelenium):
-#
-#     def test_if_login_works(self):
-#         self.login()
-#         get_info_tabs = self.selenium.find_element_by_xpath(
-#             '/html/body/div[1]/div/div/div[1]/div[1]/div[2]/ul')
-#         set_get_info_tabs = set(get_info_tabs.text.split('\n'))
-#         set_words = {'Flow', 'Location'}
-#         assert set_words in set_get_info_tabs
+from testing.test_pytest.test_base_pytest import BaseSelenium
+
+
+class MainPageTestCase(BaseSelenium):
+
+    def test_if_login_works(self):
+        self.login()
+        get_info_tabs = self.selenium.find_element_by_xpath(
+            '/html/body/div[1]/div/div/div[1]/div[1]/div[2]/ul')
+        set_get_info_tabs = set(get_info_tabs.text.split('\n'))
+        set_words = {'Flow', 'Location'}
+        assert set_words in set_get_info_tabs
 
 
 class TestClass:
@@ -21,5 +21,4 @@ class TestClass:
     def test_two(self):
         x = "hello"
         assert hasattr(x, "check")
- # pytest -q test_main_page_pytest.py
-# pytest -q testing.test_pytest.test_main_page_pytest.py
+
