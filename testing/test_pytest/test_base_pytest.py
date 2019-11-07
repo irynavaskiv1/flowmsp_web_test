23bb8b2306c9cd561f14d058a12861ee6f6a162f
Iryna <irynavaskiv875@gmail.com>
2019-11-07 14:24:17 +0200

    update for correctly pytest

--- testing/test_pytest/test_base_pytest.py
+++ testing/test_pytest/test_base_pytest.py
@@ -1,11 +1,11 @@
-import pytest
-
 from time import sleep
+
+from _pytest import unittest
 from selenium.webdriver.firefox import webdriver
 from constants import LOGIN, PASSWORD
 
 
-class BaseSelenium(pytest):
+class BaseSelenium(unittest.TestCaseFunction):
     sleep_time = 0.5
 
     def setUp(self):
@@ -32,6 +32,14 @@ class BaseSelenium(pytest):
             'form/div[3]/input')
         login_button.click()
         sleep(self.sleep_time)
+
+    def test_if_login_works(self):
+        self.login()
+        get_info_tabs = self.selenium.find_element_by_xpath(
+            '/html/body/div[1]/div/div/div[1]/div[1]/div[2]/ul')
+        set_get_info_tabs = set(get_info_tabs.text.split('\n'))
+        set_words = {'Flow', 'Location'}
+        assert set_words in set_get_info_tabs
 
     def tearDown(self):
         self.selenium.close()
